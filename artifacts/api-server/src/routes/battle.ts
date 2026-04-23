dce19c4a33a769d5f79ea2662b8cd296bc73d733
DevorHaker <devor@creature-quest.com>
2026-04-23 14:32:59 +0530

    fix: update battle type chart to official pokemon types

--- artifacts/api-server/src/routes/battle.ts
+++ artifacts/api-server/src/routes/battle.ts
@@ -21,16 +21,24 @@ const router: Router = Router();
 
 // Type chart for damage multipliers
 const TYPE_CHART: Record<string, Record<string, number>> = {
-  Fire: { Grass: 2, Crystal: 2, Air: 0.5, Water: 0.5, Earth: 0.5 },
-  Water: { Fire: 2, Earth: 2, Air: 0.5, Grass: 0.5 },
-  Grass: { Water: 2, Earth: 2, Fire: 0.5, Air: 0.5 },
-  Earth: { Fire: 2, Lightning: 2, Water: 0.5, Grass: 0.5 },
-  Air: { Earth: 2, Lightning: 2, Fire: 0.5 },
-  Lightning: { Water: 2, Air: 2, Earth: 0.5 },
-  Shadow: { Arcane: 2, Crystal: 2, Grass: 0.5 },
-  Crystal: { Shadow: 2, Grass: 2, Fire: 0.5 },
-  Arcane: { Shadow: 2, Lightning: 2 },
-  Void: { Arcane: 2, Fire: 2, Crystal: 0.5 },
+  Normal: { Rock: 0.5, Ghost: 0, Steel: 0.5 },
+  Fire: { Fire: 0.5, Water: 0.5, Grass: 2, Ice: 2, Bug: 2, Rock: 0.5, Dragon: 0.5, Steel: 2 },
+  Water: { Fire: 2, Water: 0.5, Grass: 0.5, Ground: 2, Rock: 2, Dragon: 0.5 },
+  Electric: { Water: 2, Electric: 0.5, Grass: 0.5, Ground: 0, Flying: 2, Dragon: 0.5 },
+  Grass: { Fire: 0.5, Water: 2, Grass: 0.5, Poison: 0.5, Ground: 2, Flying: 0.5, Bug: 0.5, Rock: 2, Dragon: 0.5, Steel: 0.5 },
+  Ice: { Fire: 0.5, Water: 0.5, Grass: 2, Ice: 0.5, Ground: 2, Flying: 2, Dragon: 2, Steel: 0.5 },
+  Fighting: { Normal: 2, Ice: 2, Poison: 0.5, Flying: 0.5, Psychic: 0.5, Bug: 0.5, Rock: 2, Ghost: 0, Dark: 2, Steel: 2, Fairy: 0.5 },
+  Poison: { Grass: 2, Poison: 0.5, Ground: 0.5, Rock: 0.5, Ghost: 0.5, Steel: 0, Fairy: 2 },
+  Ground: { Fire: 2, Electric: 2, Grass: 0.5, Poison: 2, Flying: 0, Bug: 0.5, Rock: 2, Steel: 2 },
+  Flying: { Electric: 0.5, Grass: 2, Fighting: 2, Bug: 2, Rock: 0.5, Steel: 0.5 },
+  Psychic: { Fighting: 2, Poison: 2, Psychic: 0.5, Dark: 0, Steel: 0.5 },
+  Bug: { Fire: 0.5, Grass: 2, Fighting: 0.5, Poison: 0.5, Flying: 0.5, Psychic: 2, Ghost: 0.5, Dark: 2, Fairy: 0.5, Steel: 0.5 },
+  Rock: { Fire: 2, Ice: 2, Fighting: 0.5, Ground: 0.5, Flying: 2, Bug: 2, Steel: 0.5 },
+  Ghost: { Normal: 0, Psychic: 2, Ghost: 2, Dark: 0.5 },
+  Dragon: { Dragon: 2, Steel: 0.5, Fairy: 0 },
+  Steel: { Fire: 0.5, Water: 0.5, Electric: 0.5, Ice: 2, Rock: 2, Steel: 0.5, Fairy: 2 },
+  Dark: { Fighting: 0.5, Psychic: 2, Ghost: 2, Dark: 0.5, Fairy: 0.5 },
+  Fairy: { Fire: 0.5, Fighting: 2, Poison: 0.5, Dragon: 2, Dark: 2, Steel: 0.5 },
 };
 
 function getTypeMultiplier(attackType: string, defType1: string, defType2?: string | null): number {
